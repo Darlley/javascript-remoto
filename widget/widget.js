c362c4f405b2aa6cb7bf7802ec79e963c8b7b368
Darlley <systembrtt@gmail.com>
2022-01-16 19:14:00 -0400

    create archives

---
+++ widget/widget.js
@@ -0,0 +1,24 @@
+var Stork = (function(window, undefined){
+  var Stork = {};
+
+  function loadSupportingFiles(callback){}
+  function getWidgetParams(){}
+  function getRatingData(params, callback){}
+  function drawWidget(){}
+
+  /*
+   *
+   */
+
+  loadSupportingFiles(function(){
+    var params = getWidgetParams();
+
+    getRatingData(params, function(){
+      drawWidget()
+    });
+
+  });
+
+  return Stork;
+
+})(window)
