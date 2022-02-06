111bbd50caf4dea849cdc54ee0bfff6ed57af4e0
Darlley <systembrtt@gmail.com>
2022-02-06 18:31:30 -0400

    capitulo 2 - adicionar script

--- widget/widget.js
+++ widget/widget.js
@@ -1,6 +1,8 @@
 var Stork = (function(window, undefined){
   var Stork = {};
 
+  console.log(window, undefined)
+
   function loadSupportingFiles(callback){}
   function getWidgetParams(){}
   function getRatingData(params, callback){}
@@ -22,3 +24,34 @@ var Stork = (function(window, undefined){
   return Stork;
 
 })(window)
+
+/*
+function loadScript(url, callback){
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = url;
+
+  var entry = document.getElementsByTagName('script')[0];
+  entry.parentNode.insertBefore(script, entry);
+
+  script.onload = script.onreadstatechange = function() {
+    var rdyState = script.readState;
+    if(!rdyState || /complete|loaded/.test(script.readyState)){
+      callback();
+      script.onload = null;
+      script.onreadystatechange = null;
+    }
+  };
+
+}
+
+loadScript('http://camerastork.com/widget/dom.js', function(){
+  Stork.dom.get('#some-id');
+});
+Stork.dom.get('#some-id');
+
+function getQueryParameters(query){
+  var args = query.split
+}
+
+*/
