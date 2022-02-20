468b81e3dc8448246dea86e50173831dc36368e3
Darlley <systembrtt@gmail.com>
2022-02-19 21:19:45 -0400

    pagina 91

--- widget/widget.js
+++ widget/widget.js
@@ -1,35 +1,62 @@
-var Stork = (function(window, undefined){
-  var Stork = {};
+function loadSupportingFiles(){
+  // carregar arquvios de apoio necessarios (p. 68)
 
-  function loadSupportingFiles(callback){
-    // carregar arquvios de apoio necessarios (p. 68)
-  }
-  function getWidgetParams(){
-    // extrair parâmetros como ID do produto ou até identificação do provedor (p. 69) 
-  }
-  function getRatingData(params, callback){
-    // Obter dados do produto (nome, URL, avaliação) a partir do servidor (p. 69) 
-  }
-  function drawWidget(){
-    // Utilizar os dados recebidos do servidor e apresentar o widget (p. 69)
-  }
+  // (p. 71)
+  var _tagScriptJquery = document.createElement('script');
+  _tagScriptJquery.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+  _tagScriptJquery.async = true;
 
-  /*
-   *
-   */
+  document.querySelectorAll('script')[0].insertAdjacentElement('beforebegin', _tagScriptJquery);
 
-  loadSupportingFiles(function(){
-    var params = getWidgetParams();
+  // "Diversas bibliotecas comumente utilizadas, conhecidas como carregadores de scripts, farão esse trabalho por você" (p. 75)
+  // LABjs, RequireJS e yepnope
+}
+loadSupportingFiles(); 
 
-    getRatingData(params, function(){
-      drawWidget()
-    });
+function getWidgetParams(){
+  // extrair parâmetros como ID do produto ou até identificação do provedor (p. 69) ~
 
-  });
+  console.log('parametros')
 
-  return Stork;
+  var elem;
+  var src;
 
-})(window)
+  var scripts = document.querySelectorAll('script');
+  scripts.forEach(item => {
+    elem = item;
+    src = elem.src;
+
+    
+    if(src && /widget\.localhost\/widget\.js/.test(src)){
+      console.log(item, elem, src)
+      return src;
+    }
+  })
+
+  return null;
+
+}
+getWidgetParams();
+
+function getRatingData(params, callback){
+  // Obter dados do produto (nome, URL, avaliação) a partir do servidor (p. 69) 
+}
+function drawWidget(){
+  // Utilizar os dados recebidos do servidor e apresentar o widget (p. 69)
+}
+
+/*
+  *
+  */
+
+// loadSupportingFiles(function(){
+//   var params = getWidgetParams();
+
+//   getRatingData(params, function(){
+//     drawWidget()
+//   });
+
+// });
 
 /*
 function loadScript(url, callback){
