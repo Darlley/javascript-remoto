cc4ff73faa6d9875299a40a4ed3e366a15dd93ee
Darlley <systembrtt@gmail.com>
2022-06-26 13:28:54 -0400

    styles from client and server widget

--- widget/widget.js
+++ widget/widget.js
@@ -1,14 +1,31 @@
-function loadSupportingFiles(){
+function getStyle(node, property, camel){
+  var value;
+  
+  if(window.getComputedStyle){
+    value = document.defaultView.getComputedStyle(node).getPropertyValue(property)
+  }else if(node.currentStyle){
+    value = node.currentStyle[property] ? node.currentStyle[property] : node.currentStyle[camel]
+  }
+
+  if(value == "" || value == "transparent" || value == "rgba(0,0,0,0)"){
+    return getStyle(node.parentNode, property, )
+  }else{
+    return value || ''
+  }
+}
+
+function loadSupportingFiles(documentHead, url){
 
   // css
   var _tagLinkStyles = document.createElement('link');
-  _tagLinkStyles.setAttribute("href", "http://widget.localhost/growp.css");
+  _tagLinkStyles.setAttribute("href", url);
   _tagLinkStyles.setAttribute("rel", "stylesheet")
   _tagLinkStyles.setAttribute("type", "text/css")
 
-  document.querySelector('head').insertAdjacentElement('beforeend', _tagLinkStyles);
+  documentHead.appendChild(_tagLinkStyles);
 }
-loadSupportingFiles(); 
+var clientHead = document.querySelector('head')
+loadSupportingFiles(clientHead, "http://widget.localhost/client-style.css"); 
 
 function getWidgetParams(){
   // create button
@@ -30,17 +47,55 @@ function getWidgetParams(){
   // criar iframe wdget
   var iframe = document.createElement('iframe')
   iframe.setAttribute('class', 'grwp grwp-iframe')
+  // estilo do container iframe
   iframe.style.width = "300px";
   iframe.style.height = "550px";
   iframe.style.border = "none";
   iframe.style.overflow = "hidden";
-  iframe.style.backgroundColor = "#006bff";
 
+  function getBasicStylesFromIframe(container, element){
+    function get(property, propertyCamel) {
+      return getStyle(container, property, propertyCamel)
+    }
+
+    var styles = {
+      color: get('color'),
+      fontFamily: get('font-family', 'fontFamily'),
+      borderColor: get('color'),
+      borderSize: "2px",
+      borderStyle: "solid",
+      margin: "0px"
+    }
+
+    return styles
+  }
+
+  function applyStyles(document, styles){
+
+    for(var property in styles){
+      if(!styles.hasOwnProperty(property)) return
+
+      document.style[property] = styles[property]
+    }
+  }
+  
   function addContentDocumentIframe(){
     var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
     var iframeBody = iframeDoc.body;
-    
-    iframeBody.innerHTML = "<h1>Darlley</h1>"
+    var iframeHead = iframeDoc.head;
+    iframeBody.setAttribute('id','growp-app')
+    iframeBody.innerHTML = ""
+
+    loadSupportingFiles(iframeHead, "http://widget.localhost/server-style.css")
+
+    // Inserir estilos no document do iframe
+    var clienteDocument = document.querySelector('body')
+    applyStyles(iframeBody, getBasicStylesFromIframe(clienteDocument))
+
+    // inserir HTML no document do iframe
+    var title = document.createElement('h1')
+    title.textContent = "Darlley"
+    iframeBody.appendChild(title)
   }
 
   // adicionar iframe ao container widget
@@ -54,6 +109,7 @@ function getWidgetParams(){
     button.classList.add('hidden')
     widget.classList.remove('hidden')
 
+    // acessar conteúdo document do iframe
     addContentDocumentIframe()
   })
   // criar evento de click no widget
